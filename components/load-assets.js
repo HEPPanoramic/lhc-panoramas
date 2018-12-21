57e8e91f0c230a7b40fa615bc8489010a2091af7
Quinnan Gill <quinnan.gill@gmail.com>
2018-12-21 13:00:50 -0700

    Hard wiring

--- components/load-assets.js
+++ components/load-assets.js
@@ -9,7 +9,7 @@ AFRAME.registerComponent('load_assets', {
 });
 
 var images = [];
-var folder = 'real_images/atlas_images/';
+var folder = 'images/';
 
 /**
  * A AJAX function that plunges into the a folder to extract the image
