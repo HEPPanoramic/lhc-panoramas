b911a44dca63000e3365ea0d83f2b1c7710f1f21
Quinnan Gill <quinnan.gill@gmail.com>
2018-12-22 10:17:44 -0700

    Jekyll compatability to the max

---
+++ _site/components/scroller.js
@@ -0,0 +1,58 @@
+/*
+ * The scroll-left and scroll-right provide a way of transitioning between
+ * excess images
+ */
+AFRAME.registerComponent('scroll-left', {
+    schema: {
+        on: {type: 'string'}
+    },
+
+    update: function () {
+        var data = this.data;
+        var el = this.el;
+
+        el.addEventListener(data.on, function() {
+            var image_groups = new ImageGroups();
+            image_groups.pull_from_storage();
+
+            var index = image_groups.get_index();
+
+            if (index > 1) {
+                console.log("New index: " + image_groups.index);
+                image_groups.set_group_prev();
+                image_groups.write_to_storage();
+            } else {
+                console.warn("Index out of range-left");
+            }
+        });
+    }
+});
+
+AFRAME.registerComponent('scroll-right', {
+    schema: {
+        on: {type: 'string'}
+    },
+
+    update: function() {
+        var data = this.data;
+        var el = this.el;
+
+        el.addEventListener(data.on, function() {
+            var image_groups = new ImageGroups();
+            image_groups.pull_from_storage();
+
+            var size = image_groups.get_size();
+            var index = image_groups.get_index();
+            
+            if (index <= size) {
+
+                image_groups.set_group_next();
+                image_groups.write_to_storage();
+            } else {
+                console.warn("Index out of range-right");
+            }
+
+            var index = image_groups.get_index();
+        });
+    }
+});
