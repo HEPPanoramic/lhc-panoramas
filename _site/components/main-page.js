f9a972085ab6714bc8d98047f291801b4ece1837
Quinnan Gill <quinnan.gill@gmail.com>
2019-03-05 09:18:00 -0500

    Made better transitions

--- _site/components/main-page.js
+++ _site/components/main-page.js
@@ -5,6 +5,22 @@ window.transitionToPage = function(href) {
     }, 500);
 }
 
-document.addEventListener('DOMContentLoaded', function(event) {
-    document.querySelector('body').style.opacity = 1;
+document.addEventListener('DOMContentLoaded', function() {
+    // Load all the components
+    const scene = document.querySelector('.scene');
+    const splash = document.querySelector('#splash');
+    const loading = document.querySelector('#splash .loading');
+
+    /*
+     * An event listner that waits for the webpage to loads then prompts the
+     * start button page
+     */
+
+    setTimeout(() => {
+        loading.style.display = 'none';
+        splash.style.backgroundColor = 'rgba(255, 255, 255, 0.0)';
+        setTimeout(() => {
+            splash.style.display = 'none';
+        }, 500);
+    }, 500);
 });
