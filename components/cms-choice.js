f8f13e2b94ec06d2d3160ea659b032f059339c18
Quinnan Gill <quinnan.gill@gmail.com>
2019-03-05 08:32:06 -0500

    Fixed CMS path issue

--- components/cms-choice.js
+++ components/cms-choice.js
@@ -2,4 +2,4 @@ var folder = 'images/cms/';
 var thumbnails = 'images/cms/';
 
 // Call makeAjaxCall which will cascade into the other functions
-makeAjaxCall(folder, folder, extractAjaxData);
+makeAjaxCall(folder, folder, thumbnails, extractAjaxData);
