952cda2604414b08ada90a9bc4867f4e63b1e46f
Quinnan Gill <quinnan.gill@gmail.com>
2019-04-02 08:32:34 -0400

    Fixed thumb scales

--- _site/image_tools/resize.py
+++ _site/image_tools/resize.py
@@ -4,8 +4,10 @@ from PIL import Image
 size = 8192, 4096
 
 for infile in sys.argv[1:]:
-    outfile = "test/" + infile.split("/")[1]
+    print infile
+    outfile = "images/test/" + infile.split("/")[-1]
+    print outfile
 
     img = Image.open(infile)
-    new_img = img.resize((8192, 4096))
+    new_img = img.resize((128, 64))
     new_img.save(outfile, "JPEG", optimize=True)
