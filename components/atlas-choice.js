56286b8891d37f9fcd4eea79fb3e817723aaa524
Quinnan Gill <quinnan.gill@gmail.com>
2019-04-02 21:44:46 -0400

    Changed path names

--- components/atlas-choice.js
+++ components/atlas-choice.js
@@ -1,5 +1,5 @@
 var folder = 'images/atlas/';
-var thumbnails = 'images/test/';
+var thumbnails = 'images/atlas_thumb/';
 
 // Call makeAjaxCall which will cascade into the other functions
 makeAjaxCall(folder, folder, thumbnails, extractAjaxData);
