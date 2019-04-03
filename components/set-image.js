a808f2d89ca95f8a3f0f4696b6a12c26d60136b3
Quinnan Gill <quinnan.gill@gmail.com>
2019-04-02 21:42:17 -0400

    Improved trans and design

--- components/set-image.js
+++ components/set-image.js
@@ -1,28 +1,27 @@
 /* global AFRAME */
 
 /**
- * Component that listens to an event, fades out an entity, swaps the texture,
- * and fades it back in.
+ * Component that listens to an event, fades out an entity, swaps the texture, and fades it
+ * back in.
  */
 AFRAME.registerComponent('set-image', {
   schema: {
     on: {type: 'string'},
     target: {type: 'selector'},
     src: {type: 'string'},
-    dur: {type: 'number', default: 700}
+    dur: {type: 'number', default: 1000}
   },
 
   init: function () {
     var data = this.data;
     var el = this.el;
 
-    //this.setupFadeAnimation();
+    this.setupFadeAnimation();
 
     el.addEventListener(data.on, function () {
       // Fade out image.
-      //data.target.emit('set-image-fade');
+      data.target.emit('set-image-fade');
       // Wait for fade to complete.
-
       setTimeout(function () {
         // Set image.
         data.target.setAttribute('material', 'src', data.src);
